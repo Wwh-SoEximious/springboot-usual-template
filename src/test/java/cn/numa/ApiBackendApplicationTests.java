1d1f2622f73ea7f7997af2112a85a4d29aa449fa
wangwenhui <wangwenhui2204@163.com>
2024-08-01 00:29:09 +0800

    初始化项目

---
+++ src/test/java/cn/numa/ApiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.numa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
